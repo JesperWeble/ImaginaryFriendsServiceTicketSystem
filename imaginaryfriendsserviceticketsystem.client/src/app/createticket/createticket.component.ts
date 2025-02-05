54a422041e3e078921d65360467cdef0e5b08333
MrSpy60 <martin-s-m@hotmail.com>
2025-02-05 12:05:27 +0100

    Create ticket component

---
+++ imaginaryfriendsserviceticketsystem.client/src/app/createticket/createticket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createticket',
+  imports: [],
+  templateUrl: './createticket.component.html',
+  styleUrl: './createticket.component.css'
+})
+export class CreateticketComponent {
+
+}
